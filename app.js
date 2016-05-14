7f7d5a83d5a91611a2205784f4ac23dc70ec9970
Alberto Coscia <alberto@albertocoscia.me>
2016-05-15 01:16:56 +0200

    Added improved products and maintenance notice

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 const pathfinding = require('pathfinding'),
     colors = require('colors');
+var optimized = true;
 
 var matrix = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -49,6 +50,35 @@ var products = [
     {x: 12, y: 9},
     {x: 12, y: 15}
 ];
+var optimizedProducts = [
+    {x: 0, y: 19},
+    {x: 0, y: 21},
+    {x: 2, y: 15},
+    {x: 2, y: 18},
+    {x: 4, y: 9},
+    {x: 4, y: 15},
+    {x: 9, y: 6},
+    {x: 9, y: 12},
+    {x: 9, y: 15},
+    {x: 7, y: 3},
+    {x: 7, y: 12},
+    {x: 7, y: 15},
+    {x: 12, y: 6},
+    {x: 12, y: 9},
+    {x: 12, y: 15},
+    {x: 12, y: 18},
+    {x: 12, y: 21},
+    {x: 15, y: 9},
+    {x: 15, y: 15},
+    {x: 15, y: 18},
+    {x: 15, y: 21},
+    {x: 19, y: 1},
+    {x: 19, y: 3},
+    {x: 19, y: 9},
+    {x: 19, y: 15}
+];
+products = optimized ? optimizedProducts : products;
+var consumers = 150;
 var grid = new pathfinding.Grid(matrix);
 displayGrid(grid);
 var finder = new pathfinding.AStarFinder({
@@ -58,13 +88,18 @@ var finder = new pathfinding.AStarFinder({
 var result = {
     cells: 0,
     money: 0,
-    rate: {}
+    rate: {},
+    ultra: 0,
+    high: 0,
+    medium: 0,
+    low: 0,
+    none: 0
 };
-for (var i = 0; i < 151; i++) {
+for (var i = 0; i <= consumers; i++) {
     createCustomer();
 }
 
-console.log(result)
+displayGrid(grid)
 displayRate(grid)
 
 
@@ -74,6 +109,7 @@ function createCustomer() {
     for (var i = 0; i < 4; i++) {
         destinations.push(products[Math.floor(Math.random() * products.length)])
     }
+    console.log(destinations)
     destinations.unshift({x: 0, y: 0});
     destinations.push({x: 19, y: 23});
     var path = createPath(destinations, grid);
@@ -131,7 +167,7 @@ function displayPath(path, grid) {
             if (node.x == path[0][0] && node.y == path[0][1]) string +=`${'AA'.bold.white.bgBlue}`;
             else if (node.x == path[path.length - 1][0] && node.y == path[path.length - 1][1]) string +=`${'BB'.bold.white.bgBlue}`;
             else if (node.isPath) string += `${'  '.black.bgBlue}`;
-            else string += `${node.walkable ? '  '.black.bgGreen : '%%'.black.bgRed}`;
+            else string += `${node.walkable ? '  '.black.bgWhite : '%%'.black.bgRed}`;
         });
         console.log(string)
     });
@@ -143,7 +179,11 @@ function displayGrid(grid) {
     grid.nodes.forEach((row) => {
         let string = '';
         row.forEach((node) => {
-            string += `${node.walkable ? '  '.black.bgGreen : '%%'.black.bgRed}`
+            products.forEach((product) => {
+                if (node.x == product.x && node.y == product.y) node.isProduct = true;
+            });
+            if (node.isProduct) string += `${'  '.black.bgBlue}`
+            else string += `${node.walkable ? '  '.black.bgWhite : '%%'.black.bgRed}`
         });
         console.log(string)
     });
@@ -157,25 +197,28 @@ function displayRate(grid) {
             if (result.rate.hasOwnProperty(`${node.x}-${node.y}`)) {
                 if (result.rate[`${node.x}-${node.y}`] > 200) {
                     string += `${'  '.black.bgRed}`;
+                    result.ultra++;
                 } else if (result.rate[`${node.x}-${node.y}`] > 150) {
                     string += `${'  '.black.bgMagenta}`;
+                    result.high++;
                 }
                 else if (result.rate[`${node.x}-${node.y}`] > 100) {
                     string += `${'  '.black.bgYellow}`;
+                    result.medium++;
                 } else if (result.rate[`${node.x}-${node.y}`] > 50) {
                     string += `${'  '.black.bgGreen}`;
+                    result.low++;
                 } else {
-                    string += `${node.walkable ? '  '.black.bgCyan : '%%'.black.bgRed}`;
+                    string += `${node.walkable ? '  '.black.bgWhite : '%%'.black.bgRed}`;
+                    if (node.walkable) result.none++;
                 }
             } else {
-                string += `${node.walkable ? '  '.black.bgCyan : '%%'.black.bgRed}`;
+                string += `${node.walkable ? '  '.black.bgWhite : '%%'.black.bgRed}`;
+                if (node.walkable) result.none++;
             }
         });
         console.log(string)
     });
-    console.log(`${'  '.black.bgRed} > 200`)
-    console.log(`${'  '.black.bgMagenta} > 150`)
-    console.log(`${'  '.black.bgYellow} > 100`)
-    console.log(`${'  '.black.bgGreen} > 50`)
-    console.log(`${'  '.black.bgCyan} < 50`)
+    console.log(`${'  '.black.bgRed} > 200: ${result.ultra}\n${'  '.black.bgMagenta} > 150: ${result.high}\n${'  '.black.bgYellow} > 100: ${result.medium}\n${'  '.black.bgGreen} > 50: ${result.low}\n${'  '.black.bgWhite} < 50: ${result.none}\n`)
+    console.log(`Average path: ${Math.floor(result.cells/consumers*100)/100}cells - Ignored surface: ${Math.floor((result.none/(result.ultra+result.high+result.medium+result.low+result.none))*10000)/100}%`)
 }
